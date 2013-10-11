e48594e88dee18e01067b15a12846daaa216404a
Alberto Hernandez <contacto.mail99@gmail.com>
2013-10-11 20:45:44 +0200

    Added True Async API

--- EPubBuilder.js
+++ EPubBuilder.js
@@ -11,11 +11,30 @@
 	zip.useWebWorkers = false;
 
 	// TODO: Check for zip.js support.
+	// TODO: Add queueClean method.
 
 	var isStringArray = function(array){
 		return array.filter(function(self){
-			return(typeof(self) !== 'string'); 
+			return(typeof(self) !== 'string');
 		}).length < 1;
+	};
+
+	var Queue = function(){
+		var queue = [];
+		var taskCallback = function(){
+			queue.shift();
+			if(queue.length > 0){
+				queue[0](taskCallback);
+			}
+		};
+		
+		new ArrayObserver(queue,function(splices){
+			if(queue.length > 0 && queue.length - splices[0].addedCount === 0){
+				queue[0](taskCallback);
+			}
+		});
+		
+		return queue;
 	};
 
 	var loadTemplates = function(templates,exclude,done){
@@ -24,7 +43,7 @@
 			return;
 		}
 
-		var exclude = exclude || [];
+		exclude = exclude || [];
 		var toLoad = templates.concat(exclude);
 
 		function loadFile(file){
@@ -36,10 +55,11 @@
 
 			xhr.onload = function(){
 				if(this.status == '200'){
-					var name = file.slice(0,(file.indexOf(".") > 0)?file.indexOf("."):file.length-1);
+					var name = file.slice(0,(file.indexOf(".") > 0)?file.indexOf("."):file.length-1),
+						template = false;
 					if(!new RegExp(exclude.join('|')).test(file)){
-						var template = Handlebars.compile(this.response);
-					}	
+						template = Handlebars.compile(this.response);
+					}
 					Book.templates[name] = template || this.response;
 					
 					// TODO: Check for objects instead of length.
@@ -49,15 +69,15 @@
 				} else {
 					var error = new Error("Book() loadTemplate: Could not load template from path ",path);
 				}
-			}
+			};
 		}
 
 		for (var i = toLoad.length - 1; i >= 0; i--) {
 			loadFile(toLoad[i]);
-		};
-	}
+		}
+	};
 
-	var loadFS = function(){
+	var createZip = function(callback){
 		// TODO: Add Error callback to Filesystem API.
 		var self =  this;
 		var fs = new zip.fs.FS();
@@ -65,9 +85,11 @@
 		var meta = fs.root.addDirectory("META-INF");
 		var book = fs.root.addDirectory("OEBPS");
 
+		self.book = book;
+
 		fs.root.addText('mimetype','application/epub+zip');
 
-		meta.addBlob('container.xml',new Blob(['<?xml version="1.0"?><container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container"><rootfiles><rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/></rootfiles></container>'],{type:'application/xml'}))
+		meta.addBlob('container.xml',new Blob(['<?xml version="1.0"?><container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container"><rootfiles><rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/></rootfiles></container>'],{type:'application/xml'}));
 
 		book.addBlob('content.opf',new Blob([Book.templates.content({
 			title:self.title,
@@ -79,26 +101,12 @@
 		})],{type:"application/oebps-package+xml"}));
 
 		book.addBlob("title_page.xhtml",new Blob([Book.templates.title_page({title:self.title,author:self.author})],{type:"application/xhtml+xml"}));
-		book.addText("style.css",Book.templates.style)
-
-		for (var i = self.chapters.length - 1; i >= 0; i--) {
-			var chapterText = Book.templates.chapter({text:self.chapters[i],index:i+1});
-			// Basic XML Parser.
-			if(!!Book.config.validateXML){
-				if(new DOMParser().parseFromString(chapterText, "application/xhtml+xml").getElementsByTagName("parsererror").length > 0){
-					throw new Error("Book(): Invalid XHTML in chapters[" + i + "]")
-				}
-			}
-
-			book.addBlob("chap" + (i+1) + ".xhtml",new Blob([],{type:"application/xhtml+xml"}));
-		};
+		book.addText("style.css",Book.templates.style);
 
 		self._zip = fs;
 
-		if(!!self._done){
-			self._done(self);
-		}
-	}
+		callback();
+	};
 
 	Handlebars.registerHelper("chapter",function(index){
 		return parseInt(index,10) + 1;
@@ -109,64 +117,98 @@
 	});
 
 	var Book = function(){
-		if(!(this instanceof Book)){
-			throw new Error("Book(): Function must be a new instance.")
+		var self = this;
+
+		if(!(self instanceof Book)){
+			throw new Error("Book(): Function must be a new instance.");
 		}
 
 		if(Object.prototype.toString.call(arguments[0]) === "[object Object]"){
 			try {
-				this.title = arguments[0].title;
-				this.author = arguments[0].author;
+				self.title = arguments[0].title;
+				self.author = arguments[0].author;
 			} catch(e){}
 
 			if('language' in arguments[0]){
 				// TODO: Add a more advanced check.
 				if(/[a-z][a-z]-[A-Z][A-Z]/g.test(arguments[0].language)){
-					this.language = arguments[0].language;
+					self.language = arguments[0].language;
 				} else {
 					throw new Error("Book(): The language parameter must comply with RFC 3066 ex:en-US.");
 				}
 			} else {
-				this.language = 'en-US';
+				self.language = 'en-US';
 			}
 
-			if('chapters' in arguments[0]){
+			self.chapters = [];
+			/*if('chapters' in arguments[0]){
 				if(!isStringArray(arguments[0].chapters)){
 					throw new Error("Book(): The Array must contain only strings.");
 				}
-				this.chapters = arguments[0].chapters;
+				self.chapters = arguments[0].chapters;
 			} else {
-				throw new Error("Book(): To create a book the parameter chapters of is required.")
-			}
+				throw new Error("Book(): To create a book the parameter chapters of is required.");
+			}*/
 		} else if(arguments[0] instanceof Array){
 			if(!isStringArray(arguments[0])){
 				throw new Error("Book(): The Array must contain only strings.");
 			}
-			this.chapters = arguments[0];
-			this.title = this.author = '';
+			self.chapters = arguments[0];
+			self.title = self.author = '';
 
 		} else {
 			throw new Error("Book(): First Argument must be an Object of Settings or an Array of Chapters.");
 		}
 
-		if(typeof(arguments[1]) == 'function'){
-			this._done = arguments[1];
-		}
+		/*if(typeof(arguments[1]) == 'function'){
+			self._done = arguments[1];
+		}*/
 
+		self._queue = new Queue();
 		// NOTE: Consider using an object instead of an array. 
-		loadTemplates(['content.opf','chapter.xhtml','title_page.xhtml'],['style.css'],loadFS.bind(this));
+		self._queue.push(loadTemplates.bind(self,['content.opf','chapter.xhtml','title_page.xhtml'],['style.css']));
+		self._queue.push(createZip.bind(self));
 	};
 
 	Book.prototype = {
 		exportBlob:function(callback){
-			this._zip.root.exportBlob(callback);
+			var self = this;
+			self._queue.push(function(){
+				self._zip.root.exportBlob(function(blob){
+					callback(blob.slice(0,blob.size,'application/epub+zip'));
+				});
+			});
+		},
+		addChapter:function(chapterText){
+			var self = this;
+			var _addChapter = function(chapterText,callback){
+				chapterText = Book.templates.chapter({text:chapterText,index:self.chapters.length});
+				// Basic XML Parser.
+				if(!!Book.config.validateXML){
+					if(new DOMParser().parseFromString(chapterText, "application/xhtml+xml").getElementsByTagName("parsererror").length > 0){
+						throw new Error("Book(): Invalid XHTML in chapters[" + self.chapters.length + "]");
+					}
+				}
+
+				self.book.addBlob("chap" + self.chapters.length + ".xhtml",new Blob([chapterText],{type:"application/xhtml+xml"}));
+				self.chapters.push(chapterText);
+
+				callback();
+			};
+
+			self._queue.push(_addChapter.bind(self,chapterText));
+		},
+		addChapters:function(chapters){
+			for (var i = 0; i < chapters.length; i++) {
+				this.addChapter(chapters[i]);
+			}
 		},
 		downloadBook:function(){
 			var self = this;
 			this._zip.root.exportBlob(function(blob){
 				window.saveAs(blob,self.title + ' - ' + self.author + '.epub');
 			});
-		}	
+		}
 	};
 
 	// TODO: Add a more advanced config function.
--- Gruntfile.js
+++ Gruntfile.js
@@ -8,45 +8,28 @@ module.exports = function(grunt) {
 		watch:{
 			files:["libs/**",'*.js','*.html'],
 			options:{
-				livereload:true
+				livereload:35721
 			}
 		},
 		connect:{
 			server:{
 				options:{
-					livereload:true
 				}
 			}
 		},
-		bump:{
-			options:{
-				files: ['package.json','bower.json'],
-				commit: true,
-				commitMessage: 'Release v%VERSION%',
-				commitFiles: ['-a'],
-				createTag:false,
-				push: true,
-				pushTo: 'origin',
-				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
-			},
-			major:{
-				createTag: true,
-				tagName: 'v%VERSION%',
-				tagMessage: 'Version %VERSION%',
+		uglify:{
+			dist:{
+				files:{
+					'EPubBuilder.min.js':['EPubBuilder.js']
+				}
 			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-usemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-bump');
 
-	grunt.renameTask("bump",'release');
-
-	grunt.registerTask('build', ['useminPrepare','concat','uglify']);
 	grunt.registerTask('server', ['connect','watch']);
 	grunt.registerTask('default', []);
 
