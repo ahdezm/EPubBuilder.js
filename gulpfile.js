56b8a68309f9677536c7d0180a242a732e3c30ff
Alberto Hernandez <contacto.mail99@gmail.com>
2014-02-08 15:22:40 +0100

    Added libs/ to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,44 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+/* jshint node:true */
+var gulp = require("gulp");
+var gutil = require("gulp-util");
 
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var connect = require("gulp-connect");
 
-var hbs = require('gulp-handlebars');
-var declare = require('gulp-declare');
+var hbs = require("gulp-handlebars");
+var declare = require("gulp-declare");
 
-var path = require('path');
+var path = require("path");
 
-gulp.task('compress',function(){
-	gulp.src('EPubBuilder.js')
+gulp.task("compress",function(){
+	gulp.src("EPubBuilder.js")
 		.pipe(uglify())
-		.pipe(rename('EPubBuilder.min.js'))
-		.pipe(gulp.dest('./'))
+		.pipe(rename("EPubBuilder.min.js"))
+		.pipe(gulp.dest("./"));
 });
 
-gulp.task('hbs',function(){
-	gulp.src('templates/*')
+gulp.task("hbs",function(){
+	gulp.src("templates/*")
 		// Add minifyed xml using pretty data
 		.pipe(hbs({
-			outputType:'bare',
+			outputType:"bare",
 			wrapped:true
 		}))
 		.pipe(declare({
-			namespace:'Book.templates',
+			namespace:"Book.templates",
 			processName:function(filePath){
-				return path.basename(filePath,path.extname(filePath))
+				return path.basename(filePath,path.extname(filePath));
 			}
 		}))
-		.pipe(concat('templates.js'))
+		.pipe(concat("templates.js"))
 		//.pipe(uglify())
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest("./"));
 });
 
-gulp.task('connect', connect.server({
-	root: __dirname + '/',
+gulp.task("connect", connect.server({
+	root: __dirname + "/",
 	port: 1337,
 	livereload: true
 }));
